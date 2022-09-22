2507917ec8bb61ffd3de7f3f5f51e23536425e0d
shervin <shervin.ses@gmail.com>
2022-09-22 11:04:12 +0330

    login form

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
   $("#menu").click(function () {
     $(this).toggleClass("fa-times");
-    $(".navbar").toggleClass("nav-toggle")
+    $(".navbar").toggleClass("nav-toggle");
   });
-  $(window).on("load scroll",function(){
-    $(this).removeClass("fa-times");
-    $(".navbar").removeClass("nav-toggle")
+  $('#login').click(function(){
+    $('.login-form').addClass('popup');
   })
+  $('.fa-times').click(function(){
+    $('.login-form').removeClass('popup');
+  })
+  $(window).on("load scroll", function () {
+    $(this).removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+    $('.login-form').removeClass('popup');
+  });
 });
