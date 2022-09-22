7fa0bfbfdaa59f9d444bbaf961a1b400aa09fbf1
shervin <shervin.ses@gmail.com>
2022-09-22 14:19:10 +0330

    about section

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $(".navbar").toggleClass("nav-toggle");
   });
-  $('#login').click(function(){
-    $('.login-form').addClass('popup');
-  })
-  $('.fa-times').click(function(){
-    $('.login-form').removeClass('popup');
-  })
+  $("#login").click(function () {
+    $(".login-form").addClass("popup");
+  });
+  $(".fa-times").click(function () {
+    $(".login-form").removeClass("popup");
+  });
   $(window).on("load scroll", function () {
     $(this).removeClass("fa-times");
     $(".navbar").removeClass("nav-toggle");
-    $('.login-form').removeClass('popup');
+    $(".login-form").removeClass("popup");
   });
 });
